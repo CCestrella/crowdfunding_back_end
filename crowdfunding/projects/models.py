b6203f258d2d8c8a2a0fc9806cc31dab4de9b315
Christin Estrella <estrella.cclm@gmail.com>
2024-10-18 12:06:19 +0800

    added model relations

--- crowdfunding/projects/models.py
+++ crowdfunding/projects/models.py
@@ -7,3 +7,13 @@ class Project(models.Model):
     image = models.URLField()
     is_open = models.BooleanField()
     date_created = models.DateTimeField(auto_now_add=True)
+
+class Pledge(models.Model):
+    amount = models.IntegerField() # The amount of money the donor is pledging
+    comment = models.CharField(max_length=200) # An optional comment from the donor
+    anonymous = models.BooleanField() # Whether the donor wants to stay anonymous
+    project = models.ForeignKey(
+        'Project', # This links the pledge to a specific project (athlete)
+        on_delete=models.CASCADE, # If the project (athlete) is deleted, all pledges will also be deleted
+        related_name='pledges' # This allows you to access all pledges linked to a specific project 
+    )
