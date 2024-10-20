bd64ff2cbebfbd22eeb8d78bac2aeda5e505892d
Christin Estrella <estrella.cclm@gmail.com>
2024-10-20 21:43:20 +0800

    added permissions

--- crowdfunding/projects/models.py
+++ crowdfunding/projects/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 class Project(models.Model):
     title = models.CharField(max_length=200)
@@ -7,6 +8,11 @@ class Project(models.Model):
     image = models.URLField()
     is_open = models.BooleanField()
     date_created = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name='owned_projects'
+    )
 
 class Pledge(models.Model):
     amount = models.IntegerField() # The amount of money the donor is pledging
@@ -15,5 +21,10 @@ class Pledge(models.Model):
     project = models.ForeignKey(
         'Project', # This links the pledge to a specific project (athlete)
         on_delete=models.CASCADE, # If the project (athlete) is deleted, all pledges will also be deleted
-        related_name='pledges' # This allows you to access all pledges linked to a specific project 
+        related_name='pledges' # This allows you to access all pledges linked to a specific project   
+    )
+    supporter = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name='pledges'
     )
--- crowdfunding/projects/tests.py
+++ crowdfunding/projects/tests.py
@@ -1,3 +1,56 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.test import TestCase
+from rest_framework.test import APIClient
+from rest_framework import status
+from django.contrib.auth.models import User
+from .models import Pledge
+
+class PledgeDetailTestCase(TestCase):
+
+    def setUp(self):
+        # Create users
+        self.user1 = User.objects.create_user(username='user1', password='password1')
+        self.user2 = User.objects.create_user(username='user2', password='password2')
+
+        # Create pledges
+        self.pledge1 = Pledge.objects.create(amount=100, supporter=self.user1)
+        self.pledge2 = Pledge.objects.create(amount=200, supporter=self.user2)
+
+        # Create API client
+        self.client = APIClient()
+
+    def test_get_pledge_as_supporter(self):
+        # Log in as user1
+        self.client.login(username='user1', password='password1')
+
+        # Attempt to get pledge1
+        response = self.client.get(f'/pledges/{self.pledge1.pk}/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_pledge_as_non_supporter(self):
+        # Log in as user2
+        self.client.login(username='user2', password='password2')
+
+        # Attempt to get pledge1
+        response = self.client.get(f'/pledges/{self.pledge1.pk}/')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_put_pledge_as_supporter(self):
+        # Log in as user1
+        self.client.login(username='user1', password='password1')
+
+        # Attempt to update pledge1
+        response = self.client.put(f'/pledges/{self.pledge1.pk}/', {'amount': 150})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.pledge1.refresh_from_db()
+        self.assertEqual(self.pledge1.amount, 150)
+
+    def test_put_pledge_as_non_supporter(self):
+        # Log in as user2
+        self.client.login(username='user2', password='password2')
+
+        # Attempt to update pledge1
+        response = self.client.put(f'/pledges/{self.pledge1.pk}/', {'amount': 150})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
