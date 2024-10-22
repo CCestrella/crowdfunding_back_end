abc94e27f555e8c7d56915893562108f2257333c
Christin Estrella <estrella.cclm@gmail.com>
2024-10-22 19:10:42 +0800

    updated requirements.txt

--- crowdfunding/crowdfunding/settings.py
+++ crowdfunding/crowdfunding/settings.py
@@ -107,8 +107,11 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+# Use DATABASE_URL if it's set
+DATABASE_URL = os.getenv('DATABASE_URL')
+
+if DATABASE_URL:
+    DATABASES['default'] = dj_database_url.config(conn_max_age=500, ssl_require=False)
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
